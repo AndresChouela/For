fcc0723f35b788465e4ef0f3a8f5dcad437a4af7
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-05-10 09:01:47 -0300

    Initial commit

---
+++ Assets/clonar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clonar : MonoBehaviour {
+
+    public int clones;
+    public int grados;
+    public GameObject prefab;
+
+    public void clonador()
+    {
+        GameObject clon;
+        for (int i = 0; i < clones; i++)
+        { 
+        clon = Instantiate(prefab);
+            clon.transform.Rotate(0, i * grados, 0);
+            clon.transform.Translate(i, i, 0);
+            //Detroy(clon, 1);
+        }
+    }
+}
